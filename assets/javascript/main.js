1fbc5bd7666266fcfd0455cf8e0561fb6262b889
Nick <nickbaker11@gmail.com>
2020-05-18 11:50:40 +0000

    timeout function added

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,6 +1,23 @@
-$(".lightsaber-handle").click(function() {
+$(".lightsaber-handle-green").click(function() {
     $(".lightsaber-green").css("height", "100px");
-    setTimeout(function(){ (".lightsaber-green").delay(1000).css;{
-       ( "height", "0px")
-    } });
+    setTimeout(function(){  $(".lightsaber-green")
+    .css("height", "0px"); }, 3000);
 });
+
+$(".lightsaber-handle-red").click(function() {
+    $(".lightsaber-red").css("height", "100px");
+    setTimeout(function(){ $(".lightsaber-red")
+    .css("height", "0px"); }, 3000);
+    });
+
+ $(".lightsaber-handle-blue").click(function() {
+    $(".lightsaber-blue").css("height", "100px");
+    setTimeout(function(){ $(".lightsaber-blue")
+    .css("height", "0px"); }, 3000);
+    });
+
+ $(".lightsaber-handle-yellow").click(function() {
+    $(".lightsaber-yellow").css("height", "100px");
+    setTimeout(function(){ $(".lightsaber-yellow")
+    .css("height", "0px"); }, 3000);
+    });
