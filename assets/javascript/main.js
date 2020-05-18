ced376e313858a8e9adc6b18389a0fea39aec672
Nick <nickbaker11@gmail.com>
2020-05-18 16:37:37 +0000

    First sequence added

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,3 +1,61 @@
+// First Sequensce
+
+$("#begin1").click(function() {
+  {
+    $("#begin1").hide()
+  }
+  $(".lightsaber-red").css("height", "100px");
+  setTimeout(function() {
+    $(".lightsaber-red")
+      .css("height", "0px");
+  }, 3000);
+
+  setTimeout(function() {
+    {
+      $(".lightsaber-green").css("height", "100px");
+      setTimeout(function() {
+        $(".lightsaber-green")
+          .css("height", "0px");
+      }, 3000)
+    }
+  }, 3000);
+
+  setTimeout(function() {
+    {
+      $(".lightsaber-blue").css("height", "100px");
+      setTimeout(function() {
+        $(".lightsaber-blue")
+          .css("height", "0px");
+      }, 3000)
+    }
+  }, 6000);
+
+  setTimeout(function() {
+    {
+      $(".lightsaber-yellow").css("height", "100px");
+      setTimeout(function() {
+        $(".lightsaber-yellow")
+          .css("height", "0px");
+      }, 3000)
+    }
+  }, 9000);
+
+  setTimeout(function() {
+    {
+      $(".lightsaber-green").css("height", "100px");
+      setTimeout(function() {
+        $(".lightsaber-green")
+          .css("height", "0px");
+      }, 3000)
+    }
+  }, 12000);
+});
+
+
+
+
+//Lightsaber activate/deactivate function
+
 $(".lightsaber-handle-green").click(function() {
     $(".lightsaber-green").css("height", "100px");
     setTimeout(function(){  $(".lightsaber-green")
@@ -21,3 +79,9 @@ $(".lightsaber-handle-red").click(function() {
     setTimeout(function(){ $(".lightsaber-yellow")
     .css("height", "0px"); }, 3000);
     });
+
+
+
+    $("#level-1-btn").click(function() {
+  $(".lightsabers").slideToggle(750)
+});
